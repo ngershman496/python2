3f58cf36dd341b4d4e2d891516034e58344caad7
ngershman496 <ngershman496@g.rwu.edu>
2022-03-01 00:39:33 -0500

    Need to include partitioning

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-#Python code for simple port scanning
+from multiprocessing.connection import wait
+import psutil
+from psutil._common import bytes2human
+import os
 
-import socket #importing library
+def show_disks():
+	temp = "%s\\ %12s %12s %12s %8d%% %12s %12s\\"
+	temp1 = "%s %12s %12s %12s %9s %12s %13s"
+	print(temp1 % ("Name", "Total", "Used", "Free", "Percent", "System", "Mount"))
+	for part in psutil.disk_partitions(all=False):
+		
+		if os.name == 'nt':
+			if 'cdrom' in part.opts or part.fstype == '':
+				continue
+			usage = psutil.disk_usage(part.mountpoint)
+			print(temp % (part.device, bytes2human(usage.total), bytes2human(usage.used),
+			  bytes2human(usage.free), int(usage.percent), part.fstype, part.mountpoint))
+
+def get_memory():
+	mem = psutil.virtual_memory()
+	temp = "%s %12s %10s %10s"
+	temp1 = "%s %13s %10s %10s"
+	print(temp % ("Total", "Avaliable", "Used", "Free"))
+	print(temp1 % (bytes2human(mem.total), bytes2human(mem.available), bytes2human(mem.used), bytes2human(mem.free)))
+
+def partition():
+	print("need to do")
 
 
-#ip = socket.gethostbyname (socket.gethostname()) #getting ip-address of host
-ip = '127.0.0.1'
-print(ip)
-for port in range(65535):	 #check for all available ports
-
-	try:
-
-		serv = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # create a new socket
-
-		serv.bind((ip,port)) # bind socket with address
-			
-	except:
-
-		print('[OPEN] Port open :',port) #print open port number
-
-	serv.close() #close connection
+if __name__ == '__main__':
+	print("Disks:")
+	show_disks()
+	print()
+	print("Memory:")
+	get_memory()
