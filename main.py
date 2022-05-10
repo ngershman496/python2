6372f91b3fd00b51311898ff8d088442b27afb9a
ngershman496 <89923898+ngershman496@users.noreply.github.com>
2022-05-10 00:18:48 -0400

    Completed Assignment 8

--- main.py
+++ main.py
@@ -1,32 +1,62 @@
-import bs4
-import requests
+import pygeoip
+import sys
 import re
 
 
-def print_presenters(arr):
-    for p in arr:
-        print(p)
+def get_info(ip):
+    get_country(ip)
+    get_city(ip)
+    get_postalcode(ip)
+    get_isp(ip)
+    get_org(ip)
 
 
-def get_presenters(arr):
-    b = []
-    for a in arr:
-        if a.find('&amp;'):
-            a = a.replace('&amp;', 'and')
-        b.append(a[6:-3])
-    return b;
+def get_rec(ip):
+    gi = pygeoip.GeoIP('GeoIPCity.dat')
+    return str(gi.record_by_addr(ip))
 
 
-def parse_site():
-    r = requests.get("https://www.blackhat.com/html/bh-media-archives/bh-archives-2000.html")
-    soup = bs4.BeautifulSoup(r.text, 'html.parser')
-    temp = soup.select('b')
-    temp = str(temp)
-    names1 = re.findall(r'2000">.*</a', temp)
-    return names1;
+def get_city(ip):
+    rec = get_rec(ip)
+    rec = re.findall('\'city\': \'.*\'latitude\'', rec)
+    rec = str(rec)
+    rec = rec[11:-15]
+    print('City: ' + rec)
+
+
+def get_country(ip):
+    rec = get_rec(ip)
+    rec = re.findall('\'country_name.*\'cont', rec)
+    rec = str(rec)
+    rec = rec[19:-10]
+    print('Country: ' + rec)
+
+
+def get_postalcode(ip):
+    rec = get_rec(ip)
+    rec = str(re.findall('\'postal_code.*\'country_code\'', rec))
+    rec = rec[18:-19]
+    print('Zip Code: ' + rec)
+
+
+def get_isp(ip):
+    gi = pygeoip.GeoIP('GeoIPISP.dat')
+    rec = str(gi.isp_by_addr(ip))
+    print('ISP: ' + rec)
+
+
+def get_org(ip):
+    gi = pygeoip.GeoIP('GeoIPOrg.dat')
+    rec = str(gi.org_by_addr(ip))
+    if rec != 'None':
+        print('ORG: ' + rec)
 
 
 if __name__ == "__main__":
-    html = parse_site()
-    presenters = get_presenters(html)
-    print_presenters(presenters)
+    if len(sys.argv) == 2:
+        if re.findall('^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$', sys.argv[1]):
+            try:
+                get_info(sys.argv[1])
+            except pygeoip.GeoIPError:
+                print('A GeoIPError has Occurred.\nExiting program')
+                exit(1)
